7f2c81b4227f38bbfd6e91787084cefcc45a6dc0
jjjuni <0210leejun@gmail.com>
2025-06-11 20:58:33 +0900

    :sparkles: 플랜 참여자 확인 API 구현

---
+++ src/main/java/com/example/travel_project/domain/plan/web/dto/IsCollaboratorResponseDTO.java
@@ -0,0 +1,10 @@
+package com.example.travel_project.domain.plan.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+@Builder
+@AllArgsConstructor
+public class IsCollaboratorResponseDTO {
+    boolean isCollaborator;
+}
